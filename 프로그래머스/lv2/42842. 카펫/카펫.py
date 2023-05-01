20cac1ad7293c646e3fc3a79fc162dfc0987138b
Michael <7471919@naver.com>
2023-05-01 15:13:10 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    
+    s = []
+    
+    for width in range(3, brown+yellow+1):
+        height = (brown+yellow)/width
+        if height % 1 == 0 and width >= height:
+            s.append([width, int(height)])
+
+    print(s)
+    for el in s:
+        if sum(el)*2 -4 == brown:
+            return el
+        
+        
+        
