e1c0c3c81fecb6b6e4131ed68cafecdf867a30c0
Michael <7471919@naver.com>
2023-05-08 14:32:11 +0900

    [level 2] Title: 전화번호 목록, Time: 97.96 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,20 @@
+# def solution(phone_book):
+#     new_book = sorted(phone_book, key=lambda x: len(x))
+    
+#     for i, e in enumerate(new_book):
+#         for v in new_book[i+1:]:
+#             if v.startswith(e):
+#                 return False
+    
+#     return True
+
+    
+def solution(phone_book):
+    # new_book = sorted(phone_book, key=lambda x: len(x))
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+            
+    return True
