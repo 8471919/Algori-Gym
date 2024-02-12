a35fae2d371ed78a4f66de91737f2a821467b8cf
Michael <7471919@naver.com>
2024-02-12 19:31:33 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 764 ms, Memory: 112384 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -2,46 +2,48 @@ import sys
 
 input = sys.stdin.readline
 
-row, col, r = map(int, input().split())
+row, col, R = map(int, input().split())
 
-mat = [list(map(int, input().split())) for _ in range(row)]
+table = [list(map(int, input().split())) for _ in range(0, row)]
 
-for _ in range(r):
-    for depth in range(min(row, col) // 2):
-        y = depth
-        x = depth
-        # 행렬의 왼쪽 위 가장 자리 요쇼
-        first = mat[y][x]
+# R번 회전
+for _ in range(0, R):
+  # 바깥쪽부터 안쪽까지 하나씩 회전
+  for depth in range(0, min(row, col) // 2):
+    x = depth
+    y = depth
+    selected = table[y][x]
 
-        # 왼쪽
-        for i in range(depth + 1, row - depth):
-            y = i
-            temp = mat[y][x]
-            mat[y][x] = first
-            first = temp
+    # 왼쪽
+    for i in range(depth+1, row-depth):
+      y = i
+      temp = table[y][x]
+      table[y][x] = selected
+      selected = temp
+    
+    # 아래
+    for i in range(depth+1, col-depth):
+      x = i
+      temp = table[y][x]
+      table[y][x] = selected
+      selected = temp
+        
+    # 오른쪽
+    for i in range(depth+1, row-depth):
+      y = row - i - 1
+      temp = table[y][x]
+      table[y][x] = selected
+      selected = temp
+    
+    # 위
+    for i in range(depth+1, col-depth):
+      x = col - i - 1
+      temp = table[y][x]
+      table[y][x] = selected
+      selected = temp
 
-        # 아래
-        for i in range(depth + 1, col - depth):
-            x = i
-            temp = mat[y][x]
-            mat[y][x] = first
-            first = temp
+for i in range(0, row):
+  for j in range(0, col):
+    print(table[i][j], end=" ")
+  print()
 
-        # 오른쪽
-        for i in range(depth + 1, row - depth):
-            y = row - i - 1
-            temp = mat[y][x]
-            mat[y][x] = first
-            first = temp
-
-        # # 위
-        for i in range(col - depth - 2, depth - 1, -1):
-            x = i
-            temp = mat[y][x]
-            mat[y][x] = first
-            first = temp
-
-for i in range(row):
-    for j in range(col):
-        print(mat[i][j], end=" ")
-    print()
